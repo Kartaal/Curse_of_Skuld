0c691553711b814d248535968f5c3a2a26e1b994
Christine <christine@milkandhoney.at>
2021-11-04 14:36:47 +0100

    PlayerInput and Controller

---
+++ curse-of-skuld/Assets/Prefabs/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float rotationSpeed = 2f;
+
+    [SerializeField]
+    private Camera camera;
+
+    private Vector3 _movementVector;
+
+    private CharacterController _charController;
+
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (_movementVector == Vector3.zero) return;
+
+        _charController.Move(transform.forward * speed * Time.deltaTime);
+
+        float _angle = Mathf.Atan2(_movementVector.x, _movementVector.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(_angle + camera.transform.eulerAngles.y, Vector3.up), Time.deltaTime * rotationSpeed);
+
+    }
+
+    //It is important the method is named this way for the input system to find it.
+    public void OnMovement(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+
+        _movementVector = new Vector3(inputVec.x, 0, inputVec.y);
+    }
+
+    //It is important the method is named this way for the input system to find it.
+    public void OnInteract()
+    {
+        print("in interact");
+    }
+}
