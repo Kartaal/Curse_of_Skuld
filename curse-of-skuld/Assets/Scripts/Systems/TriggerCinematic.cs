bf543340cb403fa219c52601c6064dd8c4deb1e0
Mahmoud <mbag@itu.dk>
2021-12-01 00:48:20 +0100

    Cinematic Trigger (not done yet)

--- curse-of-skuld/Assets/Scripts/Systems/TriggerCinematic.cs
+++ curse-of-skuld/Assets/Scripts/Systems/TriggerCinematic.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Playables;
 
 public class TriggerCinematic : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public PlayableDirector timeline;
+    
+    public Camera cam;
+    // Use this for initialization
+    void Awake()
     {
-        
+       // timeline = GetComponent<PlayableDirector>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        cam.GetComponent<Camera>().enabled = true;
+        SystemManager.Instance.playerGameObject.GetComponentInChildren<CharacterController>().enabled = false;
+        SystemManager.Instance.playerGameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+        cam.GetComponent<CinemachineVirtualCamera>().Priority = 11;
+        // timeline.Play();
+
     }
 }
