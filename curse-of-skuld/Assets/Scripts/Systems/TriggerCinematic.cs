b8c44da08ea32e755858019f4330d663a7dcf1d7
Mahmoud <mbag@itu.dk>
2021-12-01 18:37:27 +0100

    Cinematic

--- curse-of-skuld/Assets/Scripts/Systems/TriggerCinematic.cs
+++ curse-of-skuld/Assets/Scripts/Systems/TriggerCinematic.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using FMOD;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,45 +10,39 @@ using UnityEngine.InputSystem;
 using UnityEngine.Playables;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 public class TriggerCinematic : MonoBehaviour
 {
     // Start is called before the first frame update
-    public PlayableDirector timeline;
-    
-    public Camera cam;
-    [SerializeField]
-    private Volume postProcessing;
 
-    private Vignette _vignette;
-    // Use this for initialization
-    void Awake()
-    {
-        postProcessing.profile.TryGet<Vignette>(out _vignette);
-       // postProcessing.enabled = false;
-    }
+    public Camera cam;
+
 
     private void Update()
     {
-        if (cam.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Dummy"))
+        if (cam.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Finished"))
         {
-            print("fuck U");
+            cam.GetComponent<CinemachineVirtualCamera>().Priority = 1;
+            SystemManager.Instance.playerGameObject.GetComponentInChildren<CharacterController>().enabled = true;
+            SystemManager.Instance.playerGameObject.GetComponentInChildren<PlayerController>().enabled = true;
+            SystemManager.Instance.playerGameObject.GetComponentInChildren<Animator>().enabled = true;
+            //SystemManager.Instance.playerGameObject.GetComponentInChildren<MeshRenderer>().enabled = enabled;
+            Destroy(this);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         cam.GetComponent<Camera>().enabled = true;
-        postProcessing.enabled = true;
+       // volume.enabled = true;
         SystemManager.Instance.playerGameObject.GetComponentInChildren<CharacterController>().enabled = false;
-        SystemManager.Instance.playerGameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
-       // _vignette.intensity.value = math.lerp(_vignette.intensity.value, 1, Time.deltaTime * 0.2f);
+        SystemManager.Instance.playerGameObject.GetComponentInChildren<PlayerController>().enabled = false;
+        SystemManager.Instance.playerGameObject.GetComponentInChildren<Animator>().enabled = false;
+     //   SystemManager.Instance.playerGameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
         cam.GetComponent<CinemachineVirtualCamera>().Priority = 11;
         cam.GetComponent<Animator>().SetTrigger("Play");
-        // timeline.Play();
     }
-    bool AnimatorIsPlaying(){
-        return cam.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length >
-               cam.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime;
-    }
+
+ 
 }
