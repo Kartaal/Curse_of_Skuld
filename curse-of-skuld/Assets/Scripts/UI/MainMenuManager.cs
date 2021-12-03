b81879dbcdc2a64533a6313e7352eca26458a8d5
JuliusK <julius.krueger@hotmail.de>
2021-12-03 15:20:53 +0100

    Some work on main menu

---
+++ curse-of-skuld/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject controlsParent;
+    [SerializeField] private GameObject settingsParent;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Start");
+    }
+
+    public void ToggleControls()
+    {
+        Debug.Log("");
+        controlsParent.SetActive(!controlsParent.activeSelf);
+    }
+
+    public void ToggleSettings()
+    {
+        settingsParent.SetActive(!settingsParent.activeSelf);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+}
